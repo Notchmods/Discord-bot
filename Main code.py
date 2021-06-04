1edacfec0eb7b49bebba40c94d48b5c2841db066
Collin <43086357+Notchmods@users.noreply.github.com>
2021-06-04 21:46:55 +1000

    Add the reaction option
    Added the reaction option

--- Main code.py
+++ Main code.py
@@ -9,14 +9,16 @@ client = discord.Client();
 @client.event
 async def on_ready():
    print("We've logged in as {0.user}".format(client)) 
-   await client.get_channel(847833651303022645).send("I am Nathan Chiu your favorite San Francisco math teacher.")
+   await client.get_channel(847833651303022645).send("Hello I am active")
 #command terms
-Greetings = ["Hi","Hello","hello","hi","Salutations","G'day","Hello Nathan Chiu"] 
+Greetings = ["Hi","Hello","hello","hi","Salutations","G'day","Hello sir"] 
 
 Randnum = ["Random numbers","randnum","rand num","random numbers","random number","Random number"]
 color = ["Red:red_circle: ","Blue:blue_circle: ","Green:green_circle: ","Yellow :yellow_circle: ","Lime","Purple:purple_circle: ","Brown :brown_circle: ","White:white_circle: ","Black:black_circle: "]
 Goodthanks = ["I am good thanks","Good thanks","good thanks","I am good thank you very much","Thanks for asking","thanks for asking"]
-Helpneeded = ["!Help","!help","Help me Nathan Chiu","help me","Help me"]
+Helpneeded = ["!Help","!help","Help me sir","help me","Help me"]
+
+React = ["React to my message","Please react to my message","react to my message"]
 
 @client.event
 async def on_message(message):
@@ -37,5 +39,7 @@ async def on_message(message):
     await message.channel.send(random.randint(0,1000))
   if any(word in msg for word in randcolor):
     await message.channel.send(random.choice(color))
+  if any(word in msg for word in React):
+    await message.add_reaction('👍')
 keep_alive()
 client.run("your tokens")
